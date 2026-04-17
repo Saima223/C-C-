7d7e83b7c5db6ed0f1338225c07e39d5bc39b88e
Saima223 <siddiqui22062004@gmail.com>
2026-04-17 19:55:42 +0530

    started pointers basics

--- week4/day1.c
+++ week4/day1.c
@@ -1,1 +1,23 @@
-//A pointer stores the memory address of a variable. * dereferences (gets value). & gets address.
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int *ptr = &a;
+
+    printf("Value of a: %d\n", a);
+    printf("Address of a: %p\n", &a);
+
+    printf("Pointer value (address): %p\n", ptr);
+    printf("Value using pointer: %d\n", *ptr);
+
+    *ptr = 20; 
+    printf("value of a %d", a); //changed via ptr
+
+    return 0;
+    /*
+| Variable | Value stored |
+| -------- | ------------ |
+| a        | 10           |
+| ptr      | address of a |
+    */
+}
