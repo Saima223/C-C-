2a4e1dff2aa6505605be6fc5a4289bf0a2a60a70
Saima223 <siddiqui22062004@gmail.com>
2026-04-18 15:16:45 +0530

    Functions

--- week3/day1.c
+++ week3/day1.c
@@ -1,1 +1,21 @@
 // Functions avoid code repetition. Prototype (declaration), definition, call. return sends value back.
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+int sub(int a, int b){
+   return a - b;
+}
+//void function 
+void greet(){
+   printf("hey C");
+}
+
+int main(){
+   int result = add(10, 5);
+   int res = sub(20, 10);
+   printf("sum of a and b: %d\n", result);
+   greet();
+   return 0;
+}
